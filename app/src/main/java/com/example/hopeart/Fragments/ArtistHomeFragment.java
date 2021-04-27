dc05045d2cb30a78ccd49784cdef2f2fd3e2e3e9
libina153 <libinasaji@gmail.com>
2021-04-27 19:06:59 +0930

    Artist Home

--- app/src/main/java/com/example/hopeart/Fragments/ArtistHomeFragment.java
+++ app/src/main/java/com/example/hopeart/Fragments/ArtistHomeFragment.java
@@ -9,13 +9,28 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.hopeart.Adaptar.ArtistArtworkOrderAdaptar;
+import com.example.hopeart.Adaptar.ArtistHomeAdaptar;
+import com.example.hopeart.DataModel.ArtistArtWorkModel;
+import com.example.hopeart.DataModel.ArtistArtworkOrderModel;
 import com.example.hopeart.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArtistHomeFragment extends Fragment {
+    Context ctx;
+
+
     @Override
-    public void onAttach(@NonNull Context context) {
+    public void onAttach(@NonNull Context context)
+    {
         super.onAttach(context);
+        ctx=context;
     }
 
     @Nullable
@@ -28,5 +43,31 @@ public class ArtistHomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvartworkHomeOrder=view.findViewById(R.id.artworkHomeRecycleviewer);
+
+        ArtistArtWorkModel a=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+        ArtistArtWorkModel a1=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+        ArtistArtWorkModel a2=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+        ArtistArtWorkModel a3=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+        ArtistArtWorkModel a4=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+        ArtistArtWorkModel a5=new ArtistArtWorkModel("Painting","8*8","Cartridge Paper",800);
+
+
+
+        List<ArtistArtWorkModel> homelist=new ArrayList<>();
+
+        homelist.add(a);
+        homelist.add(a1);
+        homelist.add(a2);
+        homelist.add(a3);
+        homelist.add(a4);
+        homelist.add(a5);
+
+
+        ArtistHomeAdaptar homeAdaptar=new ArtistHomeAdaptar(homelist,ctx);
+        rvartworkHomeOrder.setLayoutManager(new GridLayoutManager(ctx,2,GridLayoutManager.VERTICAL,true));
+        rvartworkHomeOrder.setAdapter(homeAdaptar);
+
     }
 }
