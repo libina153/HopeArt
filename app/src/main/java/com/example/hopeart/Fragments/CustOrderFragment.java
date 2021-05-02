7bd5eaf2d027b7f1f1330eeecf74fc3e7faf09d7
truptibhat30 <tkbhat1230@gmail.com>
2021-05-02 10:08:31 -0700

    fragment,layout order

--- app/src/main/java/com/example/hopeart/Fragments/CustOrderFragment.java
+++ app/src/main/java/com/example/hopeart/Fragments/CustOrderFragment.java
@@ -9,13 +9,25 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.hopeart.Adaptar.CustCartAdapter;
+import com.example.hopeart.Adaptar.CustOrderAdapter;
+import com.example.hopeart.DataModel.CustCartModel;
+import com.example.hopeart.DataModel.CustOrderModel;
 import com.example.hopeart.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustOrderFragment extends Fragment {
+
+    Context ctx;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        ctx=context;
     }
 
     @Nullable
@@ -28,5 +40,19 @@ public class CustOrderFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvcustorder=view.findViewById(R.id.custOrderRecycleviewer);
+
+        CustOrderModel c=new CustOrderModel("1","12/02/2020","Order","img","11","5*5","Painting",200.0f);
+        CustOrderModel c1=new CustOrderModel("2","13/02/2020","pending","image","12","5*5","painting",300.00f);
+
+        List<CustOrderModel> orderlist=new ArrayList<>();
+
+        orderlist.add(c);
+        orderlist.add(c1);
+
+        CustOrderAdapter orderAdapter=new CustOrderAdapter(orderlist,ctx);
+        rvcustorder.setLayoutManager(new LinearLayoutManager(ctx, LinearLayoutManager.VERTICAL,false));
+        rvcustorder.setAdapter(orderAdapter);
     }
 }
